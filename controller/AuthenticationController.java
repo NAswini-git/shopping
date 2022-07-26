d1dc5a8f223f4494667d1f641703b53200cf2ea8
NAswini-git <aswini@thelaunchclub.in>
2022-07-26 17:11:09 +0530

    New branch_1 commit for merge check

--- controller/AuthenticationController.java
+++ controller/AuthenticationController.java
@@ -12,15 +12,21 @@ import com.shopping.service.AuthenticatorValidation;
 public class AuthenticationController {
 	private static final AuthenticatorValidation AUTHENTICATOR_VALIDATION = new AuthenticatorValidation();
 
-	public boolean validateEmailId(final String emailId) {
-		return AUTHENTICATOR_VALIDATION.validateEmailId(emailId);		
-	}
-
-	public boolean validatePassword(final String emailId,final String password) {	
-		return AUTHENTICATOR_VALIDATION.validtePassword(emailId, password);
-	}
+//	public boolean validateEmailId(final String emailId) {
+//		return AUTHENTICATOR_VALIDATION.validateEmailId(emailId);
+//	}
+//
+//	public boolean validatePassword(final String emailId,final String password) {
+//		return AUTHENTICATOR_VALIDATION.validtePassword(emailId, password);
+//	}
 
 	public void insertData(User user) {
 		AUTHENTICATOR_VALIDATION.insertData(user);	
+	}
+	public void branch2(){
+//		System.out.println("2nd time");
+	}
+	private void run(){
+//		System.out.println("hi");
 	}
 }
